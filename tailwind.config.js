31e625f6daededd94e3ed4b398e24bd64ed27124
Parmanand Parveen <115132837+Parmanand-Parveen@users.noreply.github.com>
2023-02-18 00:57:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        gg_sans:["gg-sans"]
+      }
+    
+    },
+  },
+  plugins: [],
+
+  fontFamily:{
+    Whitney:["whitney"],
+    Ginto:["ginto"],
+    ggsans:["gg-sans"]
+  }
+
+}
